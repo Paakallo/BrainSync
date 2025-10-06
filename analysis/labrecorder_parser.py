a666195dbe8b1832b1f3e5d5ce2a69f61534cd22
Sergiusz Pyskowacki <paviveerar@gmail.com>
2025-10-06 22:17:35 +0200

    no runtime errors, but tons of empty arrays

--- analysis/labrecorder_parser.py
+++ analysis/labrecorder_parser.py
@@ -7,15 +7,26 @@ from scipy.signal import welch
 from utils import *
 
 
+bands = {
+            'delta': (0.5, 4),
+            'theta': (4, 8),
+            'alpha': (8, 12),
+            'beta' : (13, 30),
+            'low_gamma': (30, 45),
+            'high_gamma': (55, 100)
+        }
+
 streams, header = pyxdf.load_xdf("453.xdf")
 sfreq = float(streams[0]["info"]["nominal_srate"][0])
 
 stream_list: list[np.array] = []
 for stream in streams:
+    if stream['info']['type'][0].lower() != 'eeg': # temp fix
+        continue
     data = np.array(stream["time_series"]).T # shape (n_channels, n_times)
     stream_list.append(data)
 
-win_size:int = 5 # seconds
+win_size:int = round(5*sfreq) # seconds
 
 all_list = [] # all_list -> streams -> channels -> sequences
 print("Streams:", len(stream_list))
@@ -25,14 +36,17 @@ for stream in stream_list:
     print("Stream: ", stream, '\n')
     for channel in range(n_channels):
         print("Channel: ", channel, '\n')
-        ch_psd = []
+        ch_seq = []
         data:np.array = stream[channel]
+        if (np.all(data==0)): continue # skip zero arrays
         n_samples:tuple = data.shape
         for i in range(n_samples[0]):
             if i+win_size == n_samples[0]: # if over last index
                 win_size-=1
             seq = data[i:i+win_size]
             psd = welch(seq, fs=sfreq) # UserWarnings may concern zero arrays in first stream
-            ch_psd.append(psd)
+            ch_seq.append(psd)
+        ch_psd = channel_psd(ch_seq, bands)
+        # ch_psd = ch_seq
         stream_psd.append(ch_psd)
     all_list.append(stream_psd)
--- analysis/utils.py
+++ analysis/utils.py
@@ -29,7 +29,7 @@ def plot_eeg_data(df, part, file_index):
     plt.show()
 
 
-def plotRawEEG(data, chart_name:str="eeg_plot"):
+def plot_raw_eeg(data, chart_name:str="eeg_plot"):
     for stream in data:
         y = stream["time_series"]
 
@@ -45,5 +45,57 @@ def plotRawEEG(data, chart_name:str="eeg_plot"):
             raise RuntimeError("Unknown stream format")
 
     plt.savefig(f"{chart_name}.png")
+
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
+
+def init_ch_waves(band_names:list):
+    ch_waves = {}
+    for band in band_names:
+        ch_waves[band] = []
+    return ch_waves
+
+
+def get_wave_values(freq:np.array, pow:np.array, band_values:tuple):
+    fmin, fmax = band_values
+    # min_idx = np.where(freq==fmin)
+    print(freq)
+    if np.all(freq==0): # temp fix
+        return pow
+    try:
+        min_idx = list(freq).index(fmin)
+    except:
+        min_idx = find_nearest(freq, fmin)
+    try:
+        max_idx = list(freq).index(fmax)
+    except:
+        max_idx = find_nearest(freq, fmax)
+    return pow[min_idx:max_idx]
+
+
+def extract_all_seq_waves(freq:np.array, pow:np.array, bands:dict):
+    seq_waves = {}
+    for band in bands.keys():
+        seq_wave = get_wave_values(freq, pow, bands[band])
+        seq_waves[band] = seq_wave
+    return seq_waves
+
+
+def channel_psd(ch_psd:list[tuple[np.array, np.array]], bands:dict):
+    ch_waves = init_ch_waves(bands.keys()) # dict of lists
+    for i, tup in enumerate(ch_psd):
+        freq, pow = tup
+        seq_waves = extract_all_seq_waves(freq, pow, bands)
+        for wave in seq_waves.keys():
+            try: # temp fix
+                val = seq_waves[wave].mean()
+            except:
+                val = seq_waves[wave]
+            ch_waves[wave].append(val)
+    return ch_waves
 
 
